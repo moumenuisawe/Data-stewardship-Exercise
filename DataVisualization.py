948c157b358b7a74cc849f35a02d8589d3028900
moumen <muisawe@gmail.com>
2019-04-14 14:30:27 +0200

    Documentation  stuff

--- DataVisualization.py
+++ DataVisualization.py
@@ -3,9 +3,14 @@ import pandas as pd
 
 # read result
 result = pd.read_csv("Data/Processed/result.csv",usecols=["year","UnEmployeeRate","movieScore"])
+
+
+plt.rcParams.update({'font.size': 18})
+
 # draw chart
 plt.plot(result["year"],result["movieScore"], label = "Movie Score")
 plt.plot(result["year"],result["UnEmployeeRate"], label = "UnEmployee  Rate")
 
 plt.legend()
+plt.savefig("reports/correlation.png")
 plt.show()
